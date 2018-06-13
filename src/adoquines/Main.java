90391c8275d7b74b1848fd8f55b833b1d7dc5fb1
Elian Zapata <elvaleli@gmail.com>
2018-06-13 07:58:40 -0500

    Random m

--- src/adoquines/Main.java
+++ src/adoquines/Main.java
@@ -19,11 +19,12 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         AdoquinVariado adoquin=new AdoquinVariado();
-        int n,m;
-        n=30;
-        m=3;
+        int n,m,lim;
+        n=10;
+        lim=(n<9)?(n-1):8;
+        m= 2 + (int)(Math.random() * lim);
         adoquin.buscarCaminos(n, m);
-        System.out.println(adoquin.cant);
+        System.out.println("n="+n+", m="+m+" Cantidad adoquines="+adoquin.cant);
         adoquin.imprimirCaminos(n, m, new Lista());
     }
     
